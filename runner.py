81e61777162d4bba028b079eb8deb1b0fb0fd15f
Pete Beardmore <pete.beardmore@msn.com>
2018-06-27 08:41:32 +0100

    [add] runner.py to call demo challenges or tests

---
+++ runner.py
@@ -0,0 +1,35 @@
+import sys
+import unittest
+from src.demo import Demo
+
+def help():
+    print("""
+syntax: %s ARG
+
+where ARG:
+  demo one [args]   : run demo one, population count
+  demo two [args]   : run demo two, max subarray
+  test              : run tests
+""" % (__file__))
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        d = Demo()
+        arg = sys.argv[1]
+        args = sys.argv[2:]
+        if arg == "test":
+            print("# running tests")
+            suite = unittest.TestLoader().discover('./test')
+            runner = unittest.TextTestRunner()
+            runner.run(suite)
+        elif arg == "demo":
+            print("# running demo")
+            d.process(args)
+        else:
+            help()
+            print("unsupported arg %r!" % (str(arg)))
+    else:
+        help()
+        print("missing arg!")
+
